0a092c682e97af920ac5c076a523920674a49a75
Alok <alok@Aloks-MacBook-Air.local>
2022-11-24 02:30:36 +0600

    most read section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,7 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'bg-image1': "url('src/Assets/Banner_img.webp')",
-      }
-    },
+    extend: {},
   },
   plugins: [require("daisyui")],
 }

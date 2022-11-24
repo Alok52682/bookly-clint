56b3107cf4654fef707c19cb3c20563583bdec83
Alok <alok@Aloks-MacBook-Air.local>
2022-11-24 11:49:33 +0600

    categories added from database

--- src/Pages/Home/Category/Category.js
+++ src/Pages/Home/Category/Category.js
@@ -1,71 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaArrowRight } from 'react-icons/fa';
 
 const Category = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:4000/categories')
+            .then(res => res.json())
+            .then(data => setCategories(data))
+    }, []);
+
     return (
         <div className='my-10 w-11/12 mx-auto p-5 border rounded-xl shadow-xl'>
             <h2 className='text-3xl text-slate-900 font-semibold'>Categories</h2>
             <div className='grid grid-cols-2 gap-3 md:grid-cols-6 items-center justify-around bg-white p-5'>
-                <div className='md:p-5 cursor-pointer h-full'>
-                    <div className='flex flex-col border rounded-lg w-full h-full'>
-                        <div className='m-4 flex justify-center items-center h-4/5'>
-                            <img src="https://i.ibb.co/k041QfY/rok-icon-school.png" className='w-full' alt="" />
-                        </div>
-                        <div className='border-t p-2 flex justify-center items-center gap-1 bg-red-600 text-white hover:text-red-600 hover:bg-slate-900 rounded-b-lg h-1/5'>
-                            <h2 className='text-xl font-semibold'>School</h2> <FaArrowRight />
-                        </div>
-                    </div>
-                </div>
-                <div className='md:p-5 cursor-pointer h-full'>
-                    <div className='flex flex-col border rounded-lg w-full h-full'>
-                        <div className='m-4 flex justify-center items-center h-4/5'>
-                            <img src="https://i.ibb.co/5M6x6Ch/rok-icon-college.png" className='w-full' alt="" />
-                        </div>
-                        <div className='border-t p-2 flex justify-center items-center gap-1 bg-red-600 text-white hover:text-red-600 hover:bg-slate-900 rounded-b-lg h-1/5'>
-                            <h2 className='text-xl font-semibold'>College</h2> <FaArrowRight />
-                        </div>
-                    </div>
-                </div>
-                <div className='md:p-5 cursor-pointer h-full'>
-                    <div className='flex flex-col border rounded-lg w-full h-full'>
-                        <div className='m-4 flex justify-center items-center h-4/5'>
-                            <img src="https://i.ibb.co/Nyvm6mv/rok-icon-university.png" className='w-full' alt="" />
-                        </div>
-                        <div className='border-t p-2 flex justify-center items-center gap-1 bg-red-600 text-white hover:text-red-600 hover:bg-slate-900 rounded-b-lg h-1/5'>
-                            <h2 className='text-xl font-semibold'>University</h2> <FaArrowRight />
-                        </div>
-                    </div>
-                </div>
-                <div className='md:p-5 cursor-pointer h-full'>
-                    <div className='flex flex-col border rounded-lg w-full h-full'>
-                        <div className='m-4 flex justify-center items-center h-4/5'>
-                            <img src="https://i.ibb.co/LxWXsxS/rok-icon-engg.png" className='w-full' alt="" />
-                        </div>
-                        <div className='border-t p-2 flex justify-center items-center gap-1 bg-red-600 text-white hover:text-red-600 hover:bg-slate-900 rounded-b-lg h-1/5'>
-                            <h2 className='text-xl font-semibold'>Engineering</h2> <FaArrowRight />
-                        </div>
-                    </div>
-                </div>
-                <div className='md:p-5 cursor-pointer h-full'>
-                    <div className='flex flex-col border rounded-lg w-full h-full'>
-                        <div className='m-4 flex justify-center items-center h-4/5'>
-                            <img src="https://i.ibb.co/s5jpMsc/rok-icon-nursing.png" className='w-full' alt="" />
-                        </div>
-                        <div className='border-t p-2 flex justify-center items-center gap-1 bg-red-600 text-white hover:text-red-600 hover:bg-slate-900 rounded-b-lg h-1/5'>
-                            <h2 className='text-xl font-semibold'>Nursing</h2> <FaArrowRight />
-                        </div>
-                    </div>
-                </div>
-                <div className='md:p-5 cursor-pointer h-full'>
-                    <div className='flex flex-col border rounded-lg w-full h-full'>
-                        <div className='m-4 flex justify-center items-center h-4/5'>
-                            <img src="https://i.ibb.co/cQ9vXL9/rok-icon-medical.png" className='w-full' alt="" />
-                        </div>
-                        <div className='border-t p-2 flex justify-center items-center gap-1 bg-red-600 text-white hover:text-red-600 hover:bg-slate-900 rounded-b-lg h-1/5'>
-                            <h2 className='text-xl font-semibold'>Medical</h2> <FaArrowRight />
-                        </div>
-                    </div>
-                </div>
+                {
+                    categories && categories.map(category => {
+                        return (
+                            <div key={category._id} className='md:p-5 cursor-pointer h-full'>
+                                <div className='flex flex-col border rounded-lg w-full h-full'>
+                                    <div className='m-4 flex justify-center items-center h-4/5'>
+                                        <img src={category.image} className='w-full' alt="" />
+                                    </div>
+                                    <div className='border-t p-2 flex justify-center items-center gap-1 bg-red-600 text-white hover:text-red-600 hover:bg-slate-900 rounded-b-lg h-1/5'>
+                                        <h2 className='text-xl font-semibold'>{category.name}</h2> <FaArrowRight />
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
             </div>
         </div>
     );

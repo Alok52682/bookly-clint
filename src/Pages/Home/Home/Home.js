367b0936c872dcd1d7d3b1fa17d4b39af99625fe
Alok <alok@Aloks-MacBook-Air.local>
2022-12-22 17:05:09 +0600

    added some component and finalize website

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -3,6 +3,7 @@ import About from '../About/About';
 import Advertise from '../Advertise/Advertise';
 import Banner from '../Banner/Banner';
 import Category from '../Category/Category';
+import Info from '../Info/Info';
 import MostRead from '../MostRead/MostRead';
 import TopSellers from '../TopSellers/TopSellers';
 
@@ -12,6 +13,7 @@ const Home = () => {
             <Banner />
             <Category />
             <Advertise />
+            <Info />
             <TopSellers />
             <MostRead />
             <About />
